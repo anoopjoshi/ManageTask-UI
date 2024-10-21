94b0c6fb4af9331ea8d782b71ebf1b53683844ea
Anoop Joshi <AJoshi@rightpoint.com>
2024-10-21 21:09:33 +0530

    perform cleanup

--- managetask-ui/src/App.tsx
+++ managetask-ui/src/App.tsx
@@ -1,16 +1,7 @@
-import TaskList from './components/manageTask';
-
-
+import TaskList from "./components/manageTask";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
-  return (
-    <>
-      {/* <h2 className="text-4xl text-center mt-10">Task Management</h2> */}
-      <TaskList />
-    </>
-  )
+  return <TaskList />;
 }
 
-export default App
+export default App;
